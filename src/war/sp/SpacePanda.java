b1d5512e35973b1dead3620c887ae9191ebffa5d
nbachman <nbachman@sslab03.cs.purdue.edu>
2013-04-24 21:46:27 -0400

    Added some bullshit!

--- src/war/sp/SpacePanda.java
+++ src/war/sp/SpacePanda.java
@@ -5,14 +5,21 @@
 
 package war.sp;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class SpacePanda {
 	
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args){
+		Scanner inputPoints = null;
+		try {
+			inputPoints = new Scanner(new FileReader(args[1]));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while(inputPoints.hasNext()){
+			String rawInput = inputPoints.nextLine();		
+		}
 	}
 }
