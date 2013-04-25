21ca188bb73802fa0450dc7211af09112f9a92de
galarnyk <galarnyk@gmail.com>
2013-04-24 20:24:31 -0400

    Add SpacePanda class for lolz

---
+++ src/war/sp/SpacePanda.java
@@ -0,0 +1,18 @@
+/**
+ * @cs348 project
+ *
+ */
+
+package war.sp;
+
+public class SpacePanda {
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
